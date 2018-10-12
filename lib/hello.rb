ab8f198bfa098f8eb756697d00b5a2691b74b2de
SANJAY K SAI <shachsan@hotmail.com>
2018-10-12 14:48:55 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
-def hello_t
-
+def hello_t(arr)
+  if block_given?
+    i = 0
+    while i < arr.length
+      yield arr[i]
+      i+=1
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
+hello_t {|name| puts name}
